9be893c2a59eb6d1badca722e729af4ecf6e7c65
ofwebdev <nkomorfaruk@gmail.com>
2023-07-15 03:44:03 +0600

    Add react-halmet-async package to handel title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { Container } from "@mui/material";
-import ProductSlider from "./components/SlideImg/v1/ProductSlider";
-import Slider from "./components/SlideImg/v1/SliderImg";
+import { Helmet } from "react-helmet-async";
 import Layout from "./layout/GlobalLayout/Layout";
+import { Container } from "@mui/material";
+
+// Components
+import Slider from "./components/SlideImg/v1/SliderImg";
+import ProductSlider from "./components/SlideImg/v1/ProductSlider";
 import Product from "./components/Product/v1/Product";
 import ViewProduct from "./components/Product/v1/ViewProduct";
 import ReuseableCarousel from "./components/ReusableProduct/ReuseableCarousel";
@@ -10,8 +13,17 @@ import ReusableProduct from "./components/ReusableProduct/RelatedProduct";
 function App() {
   return (
     <>
+      <Helmet>
+        <title>🚀 Buy Black Friday</title>
+        <meta name="description" content="Your page description" />
+        {/* Add any other meta tags or custom styles here */}
+      </Helmet>
+
+      {/* Main Layout include Header And Footer component */}
       <Layout>
+        {/* First slider   */}
         <Slider />
+
         <Container>
           <ProductSlider />
           <Product />
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React router dom
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router/router.jsx";
 
@@ -8,12 +9,16 @@ import { router } from "./router/router.jsx";
 import "react-multi-carousel/lib/styles.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+// Provider context
 import ThemeProvider from "./theme/index.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <HelmetProvider>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );

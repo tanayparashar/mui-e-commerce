24250ad48c44d2edae9ffa972ce0f200aa179645
ofwebdev <nkomorfaruk@gmail.com>
2023-11-12 06:53:54 +0600

    Add setting UI

--- src/components/Header/v1/UserSetting.jsx
+++ src/components/Header/v1/UserSetting.jsx
@@ -1,30 +1,50 @@
 import React, { useState } from "react";
-import { Box, Drawer, Typography, IconButton, Badge } from "@mui/material";
+import {
+  Box,
+  Drawer,
+  Typography,
+  IconButton,
+  Badge,
+  Divider,
+  Button,
+} from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
 import NightsStayIcon from "@mui/icons-material/NightsStay";
 import LightModeIcon from "@mui/icons-material/LightMode";
+import AlignHorizontalLeftIcon from "@mui/icons-material/AlignHorizontalLeft";
+import AlignHorizontalRightIcon from "@mui/icons-material/AlignHorizontalRight";
 import { useTheme } from "@mui/material/styles";
 // import { useThemeContext } from "../../../contexts/ThemeContext";
 
+function ReuseBtn({ onClick, icon, theme }) {
+  return (
+    <Button
+      sx={{
+        width: "50%",
+        height: "100px",
+        textAlign: "center",
+        display: "grid",
+        placeItems: "center",
+        borderRadius: "none",
+        boxShadow: theme.shadows[3],
+        "&:hover": {
+          boxShadow: theme.shadows[3], // Set the same or a different shadow for hover
+        },
+      }}
+      onClick={onClick}
+    >
+      {icon}
+    </Button>
+  );
+}
+
 function UserSetting() {
   const [openNav, setOpenNav] = useState(false);
-  // const { toggleTheme, themeMode } = useThemeContext();
+  const theme = useTheme();
 
   const toggleNav = () => {
     setOpenNav((prevOpen) => !prevOpen);
   };
-
-  // const handleDarkModeClick = () => {
-  //   if (themeMode === "light") {
-  //     toggleTheme();
-  //   }
-  // };
-
-  // const handleLightModeClick = () => {
-  //   if (themeMode === "dark") {
-  //     toggleTheme();
-  //   }
-  // };
 
   return (
     <Box sx={{ position: "relative" }}>
@@ -47,18 +67,43 @@ function UserSetting() {
           keepMounted: true,
         }}
         PaperProps={{
-          sx: { width: 340 },
+          sx: { width: 280 },
         }}
       >
-        <Box>
+        <Box sx={{ p: 1 }}>
           <Typography>Settings</Typography>
-          <Box>
-            <IconButton onClick={() => {}}>
-              <NightsStayIcon />
-            </IconButton>
-            <IconButton onClick={() => {}}>
-              <LightModeIcon />
-            </IconButton>
+          <Divider />
+          <Typography p={1}>Mode</Typography>
+
+          <Box sx={{ p: 1, display: "flex", gap: 1 }}>
+            <ReuseBtn
+              onClick={() => {}}
+              label="Night Mode"
+              icon={<NightsStayIcon />}
+              theme={theme}
+            />
+
+            <ReuseBtn
+              onClick={() => {}}
+              label="Light Mode"
+              icon={<LightModeIcon />}
+              theme={theme}
+            />
+          </Box>
+
+          <Typography p={1}>Direction</Typography>
+          <Box sx={{ p: 1, display: "flex", gap: 1 }}>
+            <ReuseBtn
+              onClick={() => {}}
+              icon={<AlignHorizontalLeftIcon />}
+              theme={theme}
+            />
+
+            <ReuseBtn
+              onClick={() => {}}
+              icon={<AlignHorizontalRightIcon />}
+              theme={theme}
+            />
           </Box>
         </Box>
       </Drawer>
--- src/page/BrokenPage/v1/ErrorPage.jsx
+++ src/page/BrokenPage/v1/ErrorPage.jsx
@@ -1,3 +1,4 @@
+import { Box, Container, Paper, Typography } from "@mui/material";
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
@@ -5,12 +6,16 @@ export default function ErrorPage() {
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <Container sx={{ textAlign: "center", mt: 5 }}>
+      <Paper sx={{ p: 4, mt: 3 }}>
+        <Typography variant="h2">Oops!</Typography>
+        <Typography variant="body1" sx={{ my: 2 }}>
+          Sorry, an unexpected error has occurred.
+        </Typography>
+        <Typography variant="subtitle1">
+          <i>{error.statusText || error.message}</i>
+        </Typography>
+      </Paper>
+    </Container>
   );
 }

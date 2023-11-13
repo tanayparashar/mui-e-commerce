4d037e48d778ad0db8e400622d91b807749a699b
ofwebdev <nkomorfaruk@gmail.com>
2023-11-13 19:53:42 +0600

    Add document route active Nav link

--- src/Docs/components/SidebarList.jsx
+++ src/Docs/components/SidebarList.jsx
@@ -1,49 +1,48 @@
-import * as React from "react";
-import {
-  List,
-  Box,
-  ListItem,
-  ListItemText,
-  Paper,
-  useTheme,
-} from "@mui/material";
-import { Link } from "react-router-dom";
+import React from "react";
+import { List, ListItem, ListItemText, useTheme } from "@mui/material";
+import { NavLink, useLocation } from "react-router-dom";
 
 export default function SidebarList({ lists }) {
   const theme = useTheme();
+  const location = useLocation();
+
   return (
-    <>
-      <List sx={{ mt: 0 }}>
-        {lists.map((list) => (
-          <ListItem
-            button
-            key={list.name}
-            component={Link}
-            to={list.path}
-            sx={{
-              px: 1,
-              py: 0.3,
-              mb: 0.5,
-              borderRadius: "6px",
-              transition: "0.5s",
-              "&:hover": {
-                backgroundColor: theme.palette.secondary.light,
-                color: theme.palette.secondary.contrastText,
-              },
+    <List sx={{ mt: 0 }}>
+      {lists.map((list) => (
+        <ListItem
+          key={list.name}
+          component={NavLink}
+          to={list.path}
+          sx={{
+            px: 1,
+            py: 0.3,
+            mb: 0.5,
+            borderRadius: "6px",
+            transition: "0.5s",
+            backgroundColor:
+              location.pathname === list.path
+                ? theme.palette.secondary.main
+                : "transparent",
+            color:
+              location.pathname === list.path
+                ? theme.palette.secondary.contrastText
+                : "inherit",
+            "&:hover": {
+              backgroundColor: theme.palette.secondary.main,
+              color: theme.palette.secondary.contrastText,
+            },
+          }}
+        >
+          <ListItemText
+            primary={list.name}
+            primaryTypographyProps={{
+              fontWeight: "500",
+              variant: "body1",
+              fontSize: "14px",
             }}
-          >
-            <ListItemText
-              to={`list.path`}
-              primary={list.name}
-              primaryTypographyProps={{
-                fontWeight: "500",
-                variant: "body1",
-                fontSize: "14px",
-              }}
-            />
-          </ListItem>
-        ))}
-      </List>
-    </>
+          />
+        </ListItem>
+      ))}
+    </List>
   );
 }
--- src/Docs/page/Setup.jsx
+++ src/Docs/page/Setup.jsx
@@ -1,14 +1,15 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 export default function Setup() {
   return (
     <div>
-      <p>
+      <Box>
         I ma the header text Lorem ipsum dolor sit amet consectetur adipisicing
         elit. Illum distinctio dolore quia. Qui, animi illo fugiat sit
         perspiciatis hic eveniet, pariatur, sequi neque molestias ipsa adipisci
         in? Incidunt, iure provident.
-      </p>
+      </Box>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
         distinctio dolore quia. Qui, animi illo fugiat sit perspiciatis hic
